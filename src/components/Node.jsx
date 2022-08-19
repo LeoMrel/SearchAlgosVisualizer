db3ccaf746b63f076f67bcb8d7329601de6fd036
Leo <leonardomrel0028@gmail.com>
2022-08-19 19:03:41 +0200

    fixed dragging bugs; refactored code

--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -1,9 +1,9 @@
-import { memo, useRef } from "react";
+import { memo, useCallback, useRef } from "react";
 import { useDrag, useDrop } from "react-dnd/dist/hooks"
 
 const Node = memo(function Node({ row, col, isStart, isEnd, updateNodes }) {
 
-    const [{ isDragging }, draggingRef] = useDrag({
+    const [, draggingRef] = useDrag({
         type: 'node',
         item: isStart ? { isStart: true } : { isStart: false },
         options: {
@@ -16,24 +16,21 @@ const Node = memo(function Node({ row, col, isStart, isEnd, updateNodes }) {
 
     const [, dropRef] = useDrop({
         accept: 'node',
-        hover: (node, monitor) => {
+        hover: useCallback((node, monitor) => {
             const isStart = monitor.getItem().isStart;
             updateNodes(isStart, row, col);
-        }
+        }, [updateNodes, row, col])
     });
-
-    const ref = useRef(null);
-    const dragRef = draggingRef(ref);
 
     return (
         <div id={`node-${ row }-${ col }`} ref={isStart || isEnd ? null : dropRef} className={`node flex place-items-center place-content-center w-8 h-8 border border-black`}>
             {isStart &&
-                <div ref={dragRef} className="start cursor-grab bg-green-500 w-full h-full flex place-content-center">
+                <div ref={draggingRef} className="start cursor-grab bg-green-500 w-full h-full flex place-content-center">
                     <i className="border-r-8 border-t-8 border-gray-800 rotate-45 h-4 w-4 place-self-center" />
                 </div>
             }
             {isEnd &&
-                <div ref={dragRef} className="end relative z-1 cursor-grab bg-red-500 w-full h-full flex place-content-center" >
+                <div ref={draggingRef} className="end relative z-1 cursor-grab bg-red-500 w-full h-full flex place-content-center" >
                     <i className="border-l-8 border-b-8 border-gray-800 rotate-45 h-4 w-4 place-self-center" />
                 </div>
             }
